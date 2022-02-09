9fe5d7032ee623f3c2bdb5959140563371c059ef
brijesh <brijeshrajbhar366@gmail.com>
2022-02-09 11:56:08 +0530

    errors resolve

--- printunlock/settings.py
+++ printunlock/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@k3m%x*t+hcu6ceq-fnxes!8-7l+8vw!ho_(p!ik5y2j$&+lk('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True 
 
 ALLOWED_HOSTS = ['127.0.0.1','https://printunlock.herokuapp.com','printunlock.herokuapp.com']
 
